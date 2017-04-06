26d7e7c2bf3f5e93f67dd820079520f693d6675e
sebpearce <sebpearce@gmail.com>
2017-04-06 21:16:12 +1000

    Add source map support

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,11 @@ module.exports = {
     path: __dirname,
   },
 
+  // allows us to see sourcemap of the js at webpack://
+  // only for dev - use just 'source-map' for prod, which creates a .map file
+  // that will be loaded by the browser only when devtools is opened
+  devtool: 'eval-source-map',
+
   module: {
     loaders: [
       {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/}
