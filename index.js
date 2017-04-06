398c7ae33f4a0459c9db2606f840243db8c6b9d2
sebpearce <sebpearce@gmail.com>
2017-04-07 08:44:47 +1000

    Create Hello World in React

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
+
+class App extends React.Component {
+  render() {
+    return <div>{'Hello world!'}</div>;
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
