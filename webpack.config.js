100747f0c18d8a81d5bda550f635392c21fb4813
sebpearce <sebpearce@gmail.com>
2017-04-10 09:13:17 +1000

    Minify JS with UglifyJsPlugin in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -8,13 +10,13 @@ module.exports = {
   // allows us to see sourcemap of the js at webpack://
   // only for dev - use just 'source-map' for prod, which creates a .map file
   // that will be loaded by the browser only when devtools is opened
-  devtool: 'eval-source-map',
+  devtool: 'source-map',
 
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       {
-        test: /\.scss/,
+        test: /\.scss$/,
         loader: [
           'style-loader',
           'css-loader?modules&sourceMap',
@@ -27,4 +29,12 @@ module.exports = {
       // { test: /\.(png|jpg|svg|gif)$/, loader: 'url', exclude: /node_modules/ },
     ],
   },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+      },
+    }),
+  ],
 };
